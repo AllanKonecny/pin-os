2e58f58fc29ce7eebf6a5fff813087a477221ab5
Allan Konečný <konecny.allan@madeo.cz>
2021-12-04 20:23:47 +0100

    cooperative multitasking

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,6 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
-use alloc::rc::Rc;
-use alloc::vec;
-use alloc::vec::Vec;
 use core::panic::PanicInfo;
 
 use bootloader::{entry_point, BootInfo};
@@ -20,6 +16,7 @@ use pine_os::allocator;
 use pine_os::memory;
 use pine_os::memory::BootInfoFrameAllocator;
 use pine_os::println;
+use pine_os::task::{executer::Executor, keyboard, Task};
 
 entry_point!(kernel_main);
 
@@ -27,6 +24,13 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
     pine_os::init();
 
+    map_heap(boot_info);
+
+    let mut executor = register_spawns();
+    executor.run();
+}
+
+fn map_heap(boot_info: &'static BootInfo) {
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
@@ -40,35 +44,13 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     }
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+}
 
-    // testing heap allocation
-
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!(
-        "current reference count is {}",
-        Rc::strong_count(&cloned_reference)
-    );
-    core::mem::drop(reference_counted);
-    println!(
-        "reference count is {} now",
-        Rc::strong_count(&cloned_reference)
-    );
-
-    // end
-
-    println!("It did not crash!");
-    pine_os::halt();
+fn register_spawns() -> Executor {
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor
 }
 
 #[cfg(not(test))]
@@ -82,4 +64,13 @@ fn panic(info: &PanicInfo) -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     pine_os::test_panic_handler(info)
+}
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
 }
